44f8f46bb25796f9efbbe9acec268771d61dbf65
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-05-02 03:45:53 +0000

    error can't log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def edit
     
   end
+  
+  def index
+    
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
