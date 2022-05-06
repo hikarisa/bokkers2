625ba20feafd3af5dceaba2f38783dcfab9ec18b
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-05-06 06:31:51 +0000

    url error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:image, :caption)
+    params.require(:book).permit(:title, :opinion)
   end
 end
