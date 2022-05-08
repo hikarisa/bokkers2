d1353ec826f08931d9a0579eb087d8359adb8122
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-05-08 14:22:53 +0000

    [add] image link

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,5 @@ class Book < ApplicationRecord
 
 
   validates :title, presence: true, length: {maximum: 200}
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum: 200}
 end
