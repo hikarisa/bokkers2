0cf5939bc2b0e1f42a3a19d3d50a4aa6b05f685d
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-05-09 06:04:21 +0000

    [add] edit validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,24 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    flash[:notice] = 'Book was successfully edit.'
   end
 
   def index
     @users = User.all
     @user = User.new
     @user = current_user
+    @book = Book.new
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(current_user.id)
+    if @user.update(user_params)
+      flash[:notice] = 'Book was successfully edit.'
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:alert] = "編集に失敗しました"
+      render :edit
+    end
   end
 
   private
