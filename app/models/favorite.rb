4f299927b376b0da72f88f0a80d2faa280b0faea
EC2 Default User <ec2-user@ip-172-31-27-234.ec2.internal>
2022-05-11 05:20:59 +0000

    [FINISH] comment favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
-  
-  
+
+  validates :user_id, presence: true, uniqueness: {scope: :book_id}
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   get 'homes/top'
   get 'home/about' => "homes#about"
   devise_for :users
+  devise_scope :users do
+    get 'signup' => 'devise/registrations#new'
+    post 'login' => 'devise/sessions#create'
+    get 'logout' => 'devise/sessions#destroy'
+  end
   root to: 'homes#top'
   resources :users, only: [:show, :edit, :index, :update]
   resources :books, only: [:edit, :index, :create, :show, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
-    
+
     resources :book_comments, only: [:create, :destroy]
   end
 end
